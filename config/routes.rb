0111b0da325c69d5469c2c25aee424fee0389533
meretamal <meretamal@uc.cl>
2021-11-22 17:07:41 -0300

    feat(functions): add relation with movie.

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
-  resources :movies
+  resources :movies do
+    resources :functions
+  end
+
+  root 'movies#index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
