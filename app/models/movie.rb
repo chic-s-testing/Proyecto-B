488425bc2af6a04a5a5416128e3b6ee93787d96b
barbaraim <birarrazaval1@uc.cl>
2021-11-24 09:56:13 -0300

    add validations for new movie

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,3 +1,23 @@
 class Movie < ApplicationRecord
     has_many :functions, dependent: :destroy
+
+    validates :name, :photo, :initialDate, :finalDate, presence: true
+    validate :valid_dates?
+
+    def valid_dates?
+        return unless initialDate? && finalDate?
+
+        if !finalDate.after?(initialDate)
+            errors.add(:initialDate, "Must be before Final Date.")
+            errors.add(:finalDate, "Must be after Initial Date.")
+        end
+
+        if !initialDate.after?(Date.today)
+            errors.add(:initialDate,"must be after today.")
+        end
+
+        if !finalDate.after?(Date.today)
+            errors.add(:finalDate,"must be after today.")
+        end
+    end
 end
