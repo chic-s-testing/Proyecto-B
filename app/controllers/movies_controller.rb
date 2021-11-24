c3a9db197abc0bc5e18655ff10187097363b6f85
javieraochoa <jiochoa@uc.cl>
2021-11-24 07:11:12 -0300

    feat(index-movies): paths to functions

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -3,7 +3,12 @@ class MoviesController < ApplicationController
 
   # GET /movies or /movies.json
   def index
-    @movies = Movie.all
+    @movies = Movie.joins(:functions)
+    @movies.each do |movie|
+      movie.functions.each do |function|
+        puts function.schedule
+      end
+    end
   end
 
   # GET /movies/1 or /movies/1.json
