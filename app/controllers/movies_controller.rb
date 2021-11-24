639444cf51e13c8a29204f0082447f2665c1c8bd
javieraochoa <jiochoa@uc.cl>
2021-11-24 07:15:04 -0300

    fix(index-movie): fix bug

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -3,12 +3,7 @@ class MoviesController < ApplicationController
 
   # GET /movies or /movies.json
   def index
-    @movies = Movie.joins(:functions)
-    @movies.each do |movie|
-      movie.functions.each do |function|
-        puts function.schedule
-      end
-    end
+    @movies = Movie.all
   end
 
   # GET /movies/1 or /movies/1.json
