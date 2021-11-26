5fdc267fb219d269a1595bc5c1055cb9c727c5e2
meretamal <meretamal@uc.cl>
2021-11-25 21:31:09 -0300

    feat(function): Add validations to function model

--- app/models/function.rb
+++ app/models/function.rb
@@ -6,6 +6,6 @@ class Function < ApplicationRecord
   validates :schedule, presence: { message: "El horario es requerido." }
   validates :room, inclusion: { in: [1, 2, 3, 4, 5, 6, 7, 8],
   message: "La sala tiene que ser un número entre 1 y 8." }
-  validates :schedule, inclusion: { in: ["Matine", "Tanda", "Noche"],
+  validates :schedule, inclusion: { in: ["matine", "tanda", "noche"],
   message: "El horario tiene que estar dentro de las opciones Matine, Tanda o Noche." }
 end
