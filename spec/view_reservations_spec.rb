ced8b192d1705b3cdda7559572f3e82600a9b8d3
Rodrigo Alonso <rialonso@uc.cl>
2021-11-28 18:23:33 -0300

    fix(reservation-tests): change dates to next year

--- spec/view_reservations_spec.rb
+++ spec/view_reservations_spec.rb
@@ -13,19 +13,19 @@ RSpec.describe 'The booking seat at function process of movie id 1', type: :feat
   it 'view matine 29 nov function, reserved seats A1 A2 A3' do
     visit '/movies/1/functions/1'
     expect(page).to have_content("Horario: matine")
-    click_link "2021-11-29"
+    click_link "2022-11-29"
     expect(page).to have_xpath("/html/body/div[1]/div[2]/form/div[1]/div[2]/text()", :text => "A1 A2 A3")
   end
   it 'view tanda 01 dic function, reserved seats B2 B3 B4' do
     visit '/movies/1/functions/2'
     expect(page).to have_content("Horario: tanda")
-    click_link "2021-12-01"
+    click_link "2022-12-01"
     expect(page).to have_xpath("/html/body/div[1]/div[2]/form/div[1]/div[2]/text()", :text => "B2 B3 B4")
   end
   it 'view noche 05 dic function, reserved seats D10 D11 D12' do
     visit '/movies/1/functions/3'
     expect(page).to have_content("Horario: noche")
-    click_link "2021-12-05"
+    click_link "2022-12-05"
     expect(page).to have_xpath("/html/body/div[1]/div[2]/form/div[1]/div[2]/text()", :text => "D10 D11 D12")
   end
 
