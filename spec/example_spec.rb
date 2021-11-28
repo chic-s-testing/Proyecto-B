c294b212a72ac5b4d9443bb88b8a6fb679c54fae
Rodrigo Alonso <rialonso@uc.cl>
2021-11-28 18:15:05 -0300

    fix(example-spec): delete file

--- spec/example_spec.rb
+++
@@ -1,9 +0,0 @@
-require 'rails_helper'
-
-# Ejemplo
-RSpec.describe 'ExampleMoviesIndex', type: :feature do
-  it 'success' do
-    visit '/movies'
-    expect(page).to have_content("Movies")
-  end
-end
